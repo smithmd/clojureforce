09db2948a9b11b377236e7e750e39f23e0338db0
Michael Smith <smithmd@interlochen.org>
2014-07-02 13:05:36 -0400

    authentication wrong

--- src/clojureforce/routes/salesforce.clj
+++ src/clojureforce/routes/salesforce.clj
@@ -16,7 +16,7 @@
   "Shows a list of available reports in salesforce"
   [request]
   (let [authentications (get-in request [:session :cemerick.friend/identity :authentications])
-        access-token (:access_token (second (first authentications)))
+        access-token (:access_token (first authentications))
         reports-response (get-salesforce-reports access-token)
         ]
     (str (vec (map :name reports-response)))
@@ -26,7 +26,7 @@
 (defn get-salesforce-reports
   "Call for authenticated salesforce user's reports"
   [access-token]
-  (let [url "https://na3.salesforce.com/services/data/v31.0/analytics/reports"
+  (let [url "https://na3.salesforce.com/services/data/v29.0/analytics/reports"
         response (client/get url {:accept :json :headers {"Authorization" (str "Bearer " access-token) }})
         reports (json/parse-string (:body response) true)]
     reports))
