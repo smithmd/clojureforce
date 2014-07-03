e53af28d8fdf7fe47f6226530dc8213c844c4297
Michael Smith <smithmd@interlochen.org>
2014-07-03 09:19:37 -0400

    created defs for api paths, added describe route

--- src/clojureforce/routes/salesforce.clj
+++ src/clojureforce/routes/salesforce.clj
@@ -13,6 +13,8 @@
 ;; data functions
 (declare get-salesforce-api-data)
 
+(def sf-base-url "https://na3.salesforce.com")
+(def sf-api-path "/services/data/v30.0")
 
 ;; Page routes
 (defn salesforce-route-authentication
@@ -25,7 +27,7 @@
 
 ;; Data for the pages
 (defn get-salesforce-api-data
-  "Get the data from a single report instance"
+  "Get data from the Salesforce API"
   [url access-token]
   (let [response (client/get url {:accept :json :headers {"Authorization" (str "Bearer " access-token)}})
         data (json/parse-string (:body response) true)]
@@ -35,14 +37,17 @@
 (defroutes salesforce-routes
   (GET "/reports/:report-id/instances/:instance-id" [report-id instance-id :as request]
     (friend/authenticated (salesforce-route-authentication request
-                            (str "https://na3.salesforce.com/services/data/v30.0/analytics/reports/" report-id "/instances/" instance-id "?includeDetails=true"))))
+                            (str sf-base-url sf-api-path "/analytics/reports/" report-id "/instances/" instance-id "?includeDetails=true"))))
   (GET "/reports/:id/instances" [id :as request]
     (friend/authenticated (salesforce-route-authentication request
-                            (str"https://na3.salesforce.com/services/data/v30.0/analytics/reports/" id "/instances"))))
+                            (str sf-base-url sf-api-path "/analytics/reports/" id "/instances"))))
+  (GET "/reports/:id/describe" [id :as request]
+    (friend/authenticated (salesforce-route-authentication request
+                            (str sf-base-url sf-api-path "/analytics/reports/" id "/describe"))))
   (GET "/reports/:id" [id :as request]
     (friend/authenticated (salesforce-route-authentication request
-                            (str "https://na3.salesforce.com/services/data/v30.0/analytics/reports/" id "?includeDetails=true"))))
+                            (str sf-base-url sf-api-path "/analytics/reports/" id "?includeDetails=true"))))
   (GET "/reports" request
     (friend/authenticated (salesforce-route-authentication request
-                            "https://na3.salesforce.com/services/data/v30.0/analytics/reports")))
+                            sf-base-url sf-api-path "/analytics/reports")))
   )
