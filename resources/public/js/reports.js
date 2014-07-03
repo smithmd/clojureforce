6cef5f7b29e60f554f6f869ffadfb17e1e1c41fb
Michael Smith <smithmd@interlochen.org>
2014-07-03 17:02:22 -0400

    moved javascript

---
+++ resources/public/js/reports.js
@@ -0,0 +1,35 @@
+var generateReportListLink = function() {
+   return '<a href="void(0);" onclick="getReportList();">Return to full report list</a>';
+};
+
+var generateReportLink = function(Object report) {
+    return '<a href="void(0);" onclick="loadReport(' + report.id + ');">' + report.name + '</a>'
+};
+
+$(document).ready( function () {
+    getReportList();
+});
+
+function getReportList() {
+    $.get('/reports', function (data) {
+        $('#getResult').html( function () {
+            var jsonArray = $.parseJSON(data);
+            var output = '';
+            $.each(jsonArray, function (index, obj) {
+                output += generateReportLink(obj) + "<br />";
+            });
+            return output;
+        });
+    });
+}
+
+function loadReport(reportId) {
+    $.get('/reports/' + reportId, function (data) {
+        $('#getResult').html( function () {
+            var output = "<p>" + generateReportListLink() + "</p>";
+            output += data;
+
+            return output;
+        });
+    });
+}
