d422f27eeeac90d63e5507e52c058dbee94f303a
Michael Smith <smithmd@interlochen.org>
2014-07-09 16:15:10 -0400

    creating divs instead of p

--- resources/public/js/reports.js
+++ resources/public/js/reports.js
@@ -37,12 +37,13 @@ function loadReport(reportId) {
 function formatReport(jsonData) {
 
     var dataset = prepData(jsonData);
+    var headers = getHeaders(jsonData);
 
-    d3.select("#chart").selectAll("p")
+    d3.select("#chart").selectAll("div")
         .data(dataset)
         .enter()
-        .append("p")
-        .text( function(d) { return d.aggregates[0].value; });
+        .append("div")
+        .classed("bar", true);
 }
 
 function prepData(jsonData) {
@@ -56,172 +57,6 @@ function prepData(jsonData) {
     return jsonArray;
 }
 
-var temporaryData = {
-                       "attributes":{
-                          "describeUrl":"/services/data/v30.0/analytics/reports/00O50000003QiypEAC/describe",
-                          "instancesUrl":"/services/data/v30.0/analytics/reports/00O50000003QiypEAC/instances",
-                          "reportId":"00O50000003QiypEAC",
-                          "reportName":"Total Revenue",
-                          "type":"Report"
-                       },
-                       "allData":true,
-                       "factMap":{
-                          "T!T":{
-                             "aggregates":[
-                                {
-                                   "label":"$43,600.00",
-                                   "value":43600
-                                },
-                                {
-                                   "label":"278",
-                                   "value":278
-                                }
-                             ]
-                          },
-                          "0!T":{
-                             "aggregates":[
-                                {
-                                   "label":"$15,400.00",
-                                   "value":15400
-                                },
-                                {
-                                   "label":"59",
-                                   "value":59
-                                }
-                             ]
-                          },
-                          "1!T":{
-                             "aggregates":[
-                                {
-                                   "label":"$28,200.00",
-                                   "value":28200
-                                },
-                                {
-                                   "label":"219",
-                                   "value":219
-                                }
-                             ]
-                          }
-                       },
-                       "groupingsAcross":{
-                          "groupings":[
+function getHeaders(jsonData) {
 
-                          ]
-                       },
-                       "groupingsDown":{
-                          "groupings":[
-                             {
-                                "groupings":[
-
-                                ],
-                                "key":"0",
-                                "label":"VIP",
-                                "value":"VIP"
-                             },
-                             {
-                                "groupings":[
-
-                                ],
-                                "key":"1",
-                                "label":"General Admission",
-                                "value":"General Admission"
-                             }
-                          ]
-                       },
-                       "hasDetailRows":false,
-                       "reportExtendedMetadata":{
-                          "aggregateColumnInfo":{
-                             "RowCount":{
-                                "acrossGroupingContext":null,
-                                "dataType":"int",
-                                "downGroupingContext":null,
-                                "label":"Record Count"
-                             },
-                             "s!Attendee__c.Total_Spent__c":{
-                                "acrossGroupingContext":null,
-                                "dataType":"currency",
-                                "downGroupingContext":null,
-                                "label":"Sum of Total Spent"
-                             }
-                          },
-                          "detailColumnInfo":{
-                             "CUST_NAME":{
-                                "dataType":"string",
-                                "label":"Festival: Festival Name"
-                             },
-                             "Festival__c.Tickets_Sold__c":{
-                                "dataType":"double",
-                                "label":"Tickets Sold"
-                             },
-                             "Festival__c.VIP_Tickets_Sold__c":{
-                                "dataType":"double",
-                                "label":"VIP Tickets Sold"
-                             },
-                             "CHILD_NAME":{
-                                "dataType":"string",
-                                "label":"Attendee: Attendee Name"
-                             },
-                             "Attendee__c.Purchase_Source__c":{
-                                "dataType":"picklist",
-                                "label":"Purchase Source"
-                             },
-                             "Attendee__c.Total_Spent__c":{
-                                "dataType":"currency",
-                                "label":"Total Spent"
-                             },
-                             "Attendee__c.Tickets_Purchased__c":{
-                                "dataType":"double",
-                                "label":"Tickets Purchased"
-                             }
-                          },
-                          "groupingColumnInfo":{
-                             "Attendee__c.Ticket_Type__c":{
-                                "dataType":"picklist",
-                                "groupingLevel":0,
-                                "label":"Ticket Type"
-                             }
-                          }
-                       },
-                       "reportMetadata":{
-                          "aggregates":[
-                             "s!Attendee__c.Total_Spent__c",
-                             "RowCount"
-                          ],
-                          "currency":null,
-                          "detailColumns":[
-                             "CUST_NAME",
-                             "Festival__c.Tickets_Sold__c",
-                             "Festival__c.VIP_Tickets_Sold__c",
-                             "CHILD_NAME",
-                             "Attendee__c.Purchase_Source__c",
-                             "Attendee__c.Total_Spent__c",
-                             "Attendee__c.Tickets_Purchased__c"
-                          ],
-                          "developerName":"Total_Revenue",
-                          "groupingsAcross":[
-
-                          ],
-                          "groupingsDown":[
-                             {
-                                "dateGranularity":"None",
-                                "name":"Attendee__c.Ticket_Type__c",
-                                "sortAggregate":null,
-                                "sortOrder":"Asc"
-                             }
-                          ],
-                          "historicalSnapshotDates":[
-
-                          ],
-                          "id":"00O50000003QiypEAC",
-                          "name":"Total Revenue",
-                          "reportBooleanFilter":null,
-                          "reportFilters":[
-
-                          ],
-                          "reportFormat":"SUMMARY",
-                          "reportType":{
-                             "label":"Custom Object with Custom Object",
-                             "type":"CustomEntityCustomEntity$Festival__c$Attendee__c"
-                          }
-                       }
-                    };
+}
