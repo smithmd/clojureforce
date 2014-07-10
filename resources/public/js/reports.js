31cd4f7e59843a40750f2ba452de06c2ef60ad86
Michael Smith <smithmd@interlochen.org>
2014-07-10 13:05:51 -0400

    centered text

--- resources/public/js/reports.js
+++ resources/public/js/reports.js
@@ -79,14 +79,15 @@ function formatReport(jsonData) {
           })
           .attr({
             x: function(d,i) {
-                return i * (w / data_set.length) + 5;
+                return i * (w / data_set.length) + (w / data_set.length - barPadding) / 2;
             },
             y: function(d) {
                 return h - Math.ceil(d.aggregates[0].value / 1000) - 2 + 15;
             },
             "font-family", "sans-serif",
             "font-size", "11px",
-            fill: "white"
+            fill: "white",
+            "text-anchor", "middle"
           });
 
 }
