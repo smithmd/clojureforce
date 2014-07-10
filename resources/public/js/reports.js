d2e207e631a724bdf32678e6bcdaf1c7009b6a05
Michael Smith <smithmd@interlochen.org>
2014-07-10 15:09:21 -0400

    messing with y values and height some more

--- resources/public/js/reports.js
+++ resources/public/js/reports.js
@@ -56,11 +56,11 @@ function formatReport(jsonData) {
             return i * (w / data_set.length);
         },
         y: function(d) {
-            return h - Math.ceil(d.aggregates[0].value / 1000) * 3;
+            return h - Math.ceil(d.aggregates[0].value / 1000) * 3 - 25;
         },
         width: (w / data_set.length) - barPadding,
         height: function(d) {
-                    return Math.ceil(d.aggregates[0].value / 1000) * 3;
+                    return Math.ceil(d.aggregates[0].value / 1000) * 3 + 25;
         },
         fill: function(d) {
                 return "rgb(0,0," + (Math.ceil(d.aggregates[0].value / 1000) * 5) + ")";
@@ -80,7 +80,7 @@ function formatReport(jsonData) {
                 return i * (w / data_set.length) + (w / data_set.length - barPadding) / 2;
             },
             y: function(d) {
-                return h - (Math.ceil(d.aggregates[0].value / 1000) * 3) + 14;
+                return h - (Math.ceil(d.aggregates[0].value / 1000) * 3) + 14 - 25;
             },
             "font-family": "sans-serif",
             "font-size": "11px",
