1bc37e135f4718b4ed9456d4886883e6362dfa68
Michael Smith <smithmd@interlochen.org>
2014-07-10 09:54:29 -0400

    attempting to draw circles of radiuses based on data

--- resources/public/js/reports.js
+++ resources/public/js/reports.js
@@ -35,18 +35,28 @@ function loadReport(reportId) {
 }
 
 function formatReport(jsonData) {
+    var w = 500;
+    var h = 100;
 
     var dataset = prepData(jsonData);
 
-    d3.select("#chart").selectAll("div")
-        .data(dataset)
-        .enter()
-        .append("div")
-        .attr("class", "bar")
-        .style("height", function(d) {
-            var heightMod = Math.ceil(d.aggregates[0].value / 1000) + 1;
-            return heightMod + "px";
-        });
+    var svg = d3.select("#chart")
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
+
+    var circles = svg.selectAll("circle")
+                     .data(dataset)
+                     .enter()
+                     .append("circle");
+
+    circles.attr("cx", function (d,i) {
+                return (i * 50) + 25;
+            })
+            .attr("cy", h/2)
+            .attr("r", function(d) {
+                return Math.ceil(d.aggregates[0].value / 1000) + 5);
+            });
 }
 
 function prepData(jsonData) {
