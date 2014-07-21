a1c3068028954d17cf4f37ff8a7991e58167834d
Michael Smith <smithmd@interlochen.org>
2014-07-21 15:29:29 -0400

    updates to bar chart

--- resources/public/js/barchart.js
+++ resources/public/js/barchart.js
@@ -6,50 +6,49 @@ function formatReport(jsonData) {
     var data_set = prepData(jsonData);
 
     var svg = d3.select("#chart")
-                .append("svg")
-                .attr("width", w)
-                .attr("height", h);
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
 
     var rects = svg.selectAll("rect")
-                   .data(data_set)
-                   .enter()
-                   .append("rect");
+        .data(data_set)
+        .enter()
+        .append("rect");
 
     rects.attr({
-        x: function(d,i) {
+        x: function (d, i) {
             return i * (w / data_set.length);
         },
-        y: function(d) {
+        y: function (d) {
             return h - Math.ceil(d.aggregates[0].value / 1000) * 3 - 25;
         },
         width: (w / data_set.length) - barPadding,
-        height: function(d) {
-                    return Math.ceil(d.aggregates[0].value / 1000) * 3 + 25;
+        height: function (d) {
+            return Math.ceil(d.aggregates[0].value / 1000) * 3 + 25;
         },
-        fill: function(d) {
-                return "rgb(0,0," + (Math.ceil(d.aggregates[0].value / 1000) * 5) + ")";
+        fill: function (d) {
+            return "rgb(0,0," + (Math.ceil(d.aggregates[0].value / 1000) * 5) + ")";
         }
     });
 
     var texts = svg.selectAll("text")
-                  .data(data_set)
-                  .enter()
-                  .append("text");
+        .data(data_set)
+        .enter()
+        .append("text");
 
-    texts.text(function(d) {
-            return Math.floor(d.aggregates[0].value / 1000);
-          })
-          .attr({
-            x: function(d,i) {
+    texts.text(function (d) {
+        return Math.floor(d.aggregates[0].value / 1000);
+    })
+        .attr({
+            x: function (d, i) {
                 return i * (w / data_set.length) + (w / data_set.length - barPadding) / 2;
             },
-            y: function(d) {
+            y: function (d) {
                 return h - (Math.ceil(d.aggregates[0].value / 1000) * 3) + 14 - 25;
             },
             "font-family": "sans-serif",
             "font-size": "11px",
             fill: "white",
             "text-anchor": "middle"
-          });
-
+        });
 }
